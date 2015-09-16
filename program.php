4fefea57de6070c365bb9420467a1f023ae8ee24
JasperDS <jasp.desmet@gmail.com>
2015-09-16 16:28:42 +0200

    program

--- program.php
+++ program.php
@@ -11,42 +11,74 @@
  * Template Name: Programma
  */
 
+
+function getProgramByGroup($groupId) {
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . "programma";
+    $sql = "SELECT datum, programma FROM " . $table_name ." WHERE afdeling = %d AND datum > NOW() ORDER BY datum;";
+    $query = $wpdb->prepare($sql, $groupId);
+    $result = $wpdb->get_results($query);
+
+    return $result;
+}
+
+$activities = [];
+if(!$_GET['id']) {
+    $activities = getProgramByGroup(17);
+} else {
+    $activities = getProgramByGroup($_GET['id']);
+}
+
+
 get_header(); ?>
 
-	<section class="container padding">
+	<section class="highlighted container padding">
 
-		<div id="kiesafdeling" class="noprint">
-        	<a href="?toon=afdeling&afdeling=1">Ribbel &#9794;</a> ||
-            <a href="?toon=afdeling&afdeling=3">Speelclub &#9794;</a> ||
-            <a href="?toon=afdeling&afdeling=5">Rakkers</a> ||
-            <a href="?toon=afdeling&afdeling=7">Toppers</a> ||
-            <a href="?toon=afdeling&afdeling=9">Kerels</a> ||
-            <a href="?toon=afdeling&afdeling=11">Aspi &#9794;</a> ||
-            <br/>
-            <a href="?toon=afdeling&afdeling=2">Ribbel &#9792;</a> ||
-            <a href="?toon=afdeling&afdeling=4">Speelclub &#9792;</a> ||
-            <a href="?toon=afdeling&afdeling=6">Kwiks</a> ||
-            <a href="?toon=afdeling&afdeling=8">Tippers</a> ||
-            <a href="?toon=afdeling&afdeling=10">Tiptiens</a> ||
-            <a href="?toon=afdeling&afdeling=12">Aspi &#9792;</a> ||
-             <br/>
-            <a href="?toon=afdeling&afdeling=15">Muziekkapel</a>
-            <!--<br/><br/>--> ||
-            <a href="?toon=afdeling&afdeling=17">Activiteiten</a>
-            <br/><br />
-            <a id="show_kalender" href="#">Bekijk per datum</a><br  />
-		</div> <!--Sluiten van 'kiesafdeling'-->
-		<div id="kalender" class="noprint">
-		<br /><hr /><br>
-  		<?php /*if ($_GET['toon']== 'datum'){*/include_once( TEMPLATEPATH . '/template-parts/calendar.php');/*}*/ ?>
-  		</div><!--#kalender-->
+        <div class="row">
 
-        <?php while ( have_posts() ) : the_post(); ?>
+            <div class="col-md-6">
 
-            <?php get_template_part( 'template-parts/content', 'page' ); ?>
+                <div id="kiesafdeling" class="noprint">
+                    <a href="?id=1">Ribbel &#9794;</a> ||
+                    <a href="?id=3">Speelclub &#9794;</a> ||
+                    <a href="?id=5">Rakkers</a> ||
+                    <a href="?id=7">Toppers</a> ||
+                    <a href="?id=9">Kerels</a> ||
+                    <a href="?id=11">Aspi &#9794;</a> ||
+                    <br/>
+                    <a href="?id=2">Ribbel &#9792;</a> ||
+                    <a href="?id=4">Speelclub &#9792;</a> ||
+                    <a href="?id=6">Kwiks</a> ||
+                    <a href="?id=8">Tippers</a> ||
+                    <a href="?id=10">Tiptiens</a> ||
+                    <a href="?id=12">Aspi &#9792;</a> ||
+                    <a href="?id=17">Activiteiten</a>
+                </div>
 
-        <?php endwhile; // End of the loop. ?>
+            </div>
+
+            <!-- <div class="col-md-6">
+                <div id="kalender" class="noprint">
+                    <?php /*if ($_GET['toon']== 'datum'){*/include_once( TEMPLATEPATH . '/template-parts/calendar.php');/*}*/ ?>
+                </div>
+            </div> -->
+
+        </div>
 
 	</section>
+
+    <section class="container">
+
+        <?php
+            foreach($activities as $activity) {
+                echo '<div class="activity">';
+                    echo '<div class="date">' . date_i18n(get_option( 'date_format' ), strtotime($activity->datum)) . '</div>';
+                    echo '<div class="program">' . nl2br($activity->programma) . '</div>';
+                echo '</div>';
+            }
+        ?>
+
+    </section>
 
 <?php get_footer(); ?>
