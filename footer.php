648ad5504404de6c06823006fcdeef4024ad6a25
Mante Bridts <mantebridts@gmail.com>
2018-05-01 17:12:13 +0200

    Updated btn from EN to NL

--- footer.php
+++ footer.php
@@ -42,7 +42,7 @@
 									<div class="response" id="mce-success-response" style="display:none"></div>
 								</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 								<div style="position: absolute; left: -5000px;"><input type="text" name="b_64d23a3209a2640ce2e7f37d1_f572872947" tabindex="-1" value=""></div>
-								<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+								<div class="clear"><input type="submit" value="Schrijf in" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 								</div>
 							</form>
 						</div>
